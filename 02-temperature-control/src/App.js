47bae2da95c66b00efed2898c28ba0e257ff249c
Erhan Balık <erhanbalik93@gmail.com>
2022-04-22 03:05:26 +0300

    Temp Controller

--- 02-temperature-control/src/App.js
+++ 02-temperature-control/src/App.js
@@ -1,9 +1,40 @@
-
+import {useState} from 'react';
 
 function App() {
+
+    const [temp, setTemp] = useState(25);
+    const [tempColor, setTempColor] = useState('cold');
+
+    const incTemp = () => {
+        if( temp === 35) return;
+        const newTemp = temp + 1;
+        setTemp(newTemp);
+
+        if (newTemp >= 28){
+            setTempColor('hot');
+        }
+    }
+
+    const decTemp = () => {
+        if( temp === 15) return;
+        const newTemp = temp - 1;
+        setTemp(newTemp);
+
+        if (newTemp <= 26){
+            setTempColor('cold');
+        }
+    }
+
+
   return (
     <div className="App">
-      <h2>Temperature Control</h2>
+      <h2>Temperature Control App</h2>
+        <div className={`temp-display ${tempColor}`}>{temp} ˚C</div>
+        <div className='btn-container'>
+            <button onClick={() => decTemp()}>-</button>
+            <button onClick={() => incTemp()}>+</button>
+        </div>
+
     </div>
   );
 }
